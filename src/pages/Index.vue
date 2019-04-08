f630cef0f8867a02a41f4ceb33ffdfc89cc39d5b
Evan Sparkman <me@evansparkman.com>
2019-04-08 11:09:22 -0400

    Update Post template

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <Pager :info="$page.posts.pageInfo" />
-    <div v-for="post in $page.posts.edges" :key="post.id" class="max-w rounded overflow-hidden shadow-lg mt-8">
+    <div v-for="post in $page.posts.edges" :key="post.id" :to="post.node.path" class="max-w rounded overflow-hidden shadow-lg mt-8">
       <img :src="post.node.coverImage" class="w-full h-48 object-cover"/>
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">{{post.node.title}}</div>
@@ -22,6 +22,7 @@
           </g-link>
         </div>
       </div>
+      <g-link class="post__link" :to="post.node.path">Link</g-link>
     </div>
   </Layout>
 </template>
@@ -59,7 +60,15 @@ export default {
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
+.post__link {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.0;
+  overflow: hidden;
+  text-indent: -9999px;
+  z-index: 0;
 }
 </style>
--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,15 +1,45 @@
 <template>
   <Layout>
-    <h1 v-html="$page.post.title"/>
-    <div v-html="$page.post.content"/>
+    <div class="mt-8">
+      <h1 class="pb-1 text-xl font-bold">
+        {{ $page.post.title }}
+      </h1>
+      <div class="my-4">
+        Posted {{ $page.post.date }}
+        <template v-if="$page.post.timeToRead">
+          <strong>{{ $page.post.timeToRead }} min read.</strong>  
+        </template>  
+      </div>
+      <!-- <PostMeta :post="$page.post" /> -->
+
+    </div>
+    
+    <div class="post content-box">
+      <div class="post__header">
+        <img alt="Cover image" class="w-full h-48 object-cover" v-if="$page.post.coverImage" :src="$page.post.coverImage" />
+      </div>
+
+      <div class="mt-8" v-html="$page.post.content" />
+
+      <div class="post__footer">
+        <!-- <PostTags :post="$page.post" /> -->
+      </div>
+    </div>
+
+    <!-- <Author class="post-author" /> -->
   </Layout>
 </template>
 
 <page-query>
 query Post($path: String!) {
-  post(path: $path) {
+  post: post(path: $path) {
     title
+    path
+    date (format: "MMM D YYYY")
+    timeToRead
+    tags
     content
+    coverImage (height: 320, quality: 100)
   }
 }
 </page-query>
@@ -17,3 +47,52 @@ query Post($path: String!) {
 <script>
 export default {}
 </script>
+
+<style lang="scss">
+.post-title {
+  padding: calc(var(--space) / 2) 0 calc(var(--space) / 2);
+  text-align: center;
+}
+.post {
+  &__header {
+    width: calc(100% + var(--space) * 2);
+    margin-left: calc(var(--space) * -1);
+    margin-top: calc(var(--space) * -1);
+    margin-bottom: calc(var(--space) / 2);
+    overflow: hidden;
+    border-radius: var(--radius) var(--radius) 0 0;
+    
+    img {
+      width: 100%;
+    }
+    &:empty {
+      display: none;
+    }
+  }
+  &__content {
+    h2:first-child {
+      margin-top: 0;
+    }
+    p:first-of-type {
+      font-size: 1.2em;
+      color: var(--title-color);
+    }
+    img {
+      width: calc(100% + var(--space) * 2);
+      margin-left: calc(var(--space) * -1);
+      display: block;
+      max-width: none;
+    }
+  }
+}
+.post-comments {
+  padding: calc(var(--space) / 2);
+  
+  &:empty {
+    display: none;
+  }
+}
+.post-author {
+  margin-top: calc(var(--space) / 2);
+}
+</style>
